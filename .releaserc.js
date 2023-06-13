7157d7fb7a327c21fc7659571fb5e2b20cb9590c
ryajjala <rajasekhar19489@gmail.com>
2023-06-13 22:00:16 +0530

    Update .releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ['origin/release'],
+  branches: ['release'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
